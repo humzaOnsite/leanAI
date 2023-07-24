f47c4d26ae63e656465eed04dde19724290a05af
humzaOnsite <139470836+humzaOnsite@users.noreply.github.com>
2023-07-24 09:09:26 -0400

    Add Number field

--- DotNetCoreSqlDb/Models/Todo.cs
+++ DotNetCoreSqlDb/Models/Todo.cs
@@ -12,5 +12,7 @@ namespace DotNetCoreSqlDb.Models
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreatedDate { get; set; }
+
+        public int Number { get; set; }
     }
 }
