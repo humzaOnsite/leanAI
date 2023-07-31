3bb933a18c30192a6343c8972cb37669fac7099e
humzaOnsite <139470836+humzaOnsite@users.noreply.github.com>
2023-07-31 09:16:02 -0400

    Use Part 8 (add new field) Microsoft tutorial

--- DotNetCoreSqlDb/Models/Todo.cs
+++ DotNetCoreSqlDb/Models/Todo.cs
@@ -7,13 +7,12 @@ namespace DotNetCoreSqlDb.Models
     {
         public int ID { get; set; }
         public string? Description { get; set; }
-        public int Number { get; set; }
 
         [DisplayName("Created Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreatedDate { get; set; }
 
-
+        public int Number { get; set; }
     }
 }
