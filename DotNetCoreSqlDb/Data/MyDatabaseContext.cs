11c2c94aee04b38b9f356a313e086ac1ba074573
humzaOnsite <humza@onsitesmart.works>
2023-08-01 15:21:10 +0000

    Using Part 8 Guide & ChatGPT

--- DotNetCoreSqlDb/Data/MyDatabaseContext.cs
+++ DotNetCoreSqlDb/Data/MyDatabaseContext.cs
@@ -9,11 +9,19 @@ namespace DotNetCoreSqlDb.Data
 {
     public class MyDatabaseContext : DbContext
     {
-        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
+        public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options)
             : base(options)
         {
         }
 
         public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=tcp:leanai-server.database.windows.net,1433;Initial Catalog=leanai;Persist Security Info=False;User ID=leanai-server-admin;Password=leanmfga!4;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
     }
 }
