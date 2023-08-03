217235809f918ccbfbc1bf2f3e3ad5042c777591
humzaOnsite <humza@onsitesmart.works>
2023-08-03 14:17:49 +0000

    Attempt with dotnet build

--- DotNetCoreSqlDb/Migrations/20230803141341_InitialCreate.Designer.cs
+++ DotNetCoreSqlDb/Migrations/20230803141341_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetCoreSqlDb.Migrations
 {
     [DbContext(typeof(MyDatabaseContext))]
-    [Migration("20230731134155_InitialCreate")]
+    [Migration("20230803141341_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -20,7 +20,7 @@ namespace DotNetCoreSqlDb.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.5")
+                .HasAnnotation("ProductVersion", "7.0.9")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
